b168f333418bc145efda4eab702dff51be92a37b
mhamed_hattab <mhamed.httb@gmail.com>
2016-05-23 16:20:30 +0100

    add gitignore

--- underServer/insert.php
+++ underServer/insert.php
@@ -8,7 +8,7 @@ $prenom=$_GET['prenom'];
 $sql = "INSERT INTO personne ( nom, prenom)
 VALUES ( '$nom','$prenom')";
 
-if (mysqli_query($conn, $sql)) {
+if (mysqli_query($conn, $sql)) { 
     echo "successfully added";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
